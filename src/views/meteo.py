a15ec98b84f0346f59b8f7c78ed2af1e1e7569e7
marieghr <marieghr27@gmail.com>
2025-07-03 13:07:41 +0100

    meteo police et couleur

--- src/views/meteo.py
+++ src/views/meteo.py
@@ -22,7 +22,7 @@ geopoint = df_ville.iloc[0][["geopoint"]].values if df_ville is not None else No
 params = {
     "key": "2fbbaab3e35841d5ba0125329251906",
     "q": geopoint,
-    "days": 7,
+    "days": 3,
     "alerts": "yes",
 }
 
@@ -33,7 +33,7 @@ data = response2.json()
 
 st.markdown(
     f"""
-    <h1 style='text-align: center; color: dodgerblue;'>
+    <h1 style='text-align: center; color: #57bb8a;font-family: "Economica";'>
         Le point météo de {ville_cookie if ville_cookie else "ta ville"}
     </h1>
     """,
@@ -107,32 +107,65 @@ col1, col2, col3, col4 = st.columns(4)
 with col2:
     st.write("")
     st.write("")
-    st.html("""<h2 style='text-align: center; color: white' > Aujourd'hui </h2>""")
+    st.html("""<h2 style='text-align: center; color: white;font-family: "Economica"' > Aujourd'hui </h2>""")
 
 with col3:
     st.image(df_meteo["condition_icon"][0], width=150)
 
 
+# Inclure la police Open Sans depuis Google Fonts et definir les metrics et titres
+st.markdown("""
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap');
+        .metric-title {
+            font-family: 'Open Sans', sans-serif;
+            font-size: 16px;
+            font-weight: 600;
+            text-align: center;
+        }
+        .metric-value {
+            font-family: 'Open Sans', sans-serif;
+            font-size: 20px;
+            font-weight: 400;
+            text-align: center;
+        }
+        .stMetric {
+            text-align: center;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+# Création des 4 colonnes
 col1, col2, col3, col4 = st.columns(4)
 
+# Affichage des éléments dans chaque colonne
 with col1:
-    st.metric("Temperature max", f"{df_meteo["max_temp"][0]} °C")
+    st.markdown('<p class="metric-title">Température max</p>', unsafe_allow_html=True)
+    st.markdown(f'<p class="metric-value">{df_meteo["max_temp"][0]} °C</p>', unsafe_allow_html=True)
+
 with col2:
-    st.metric("Temperature min", f"{df_meteo["min_temp"][0]} °C")
+    st.markdown('<p class="metric-title">Température min</p>', unsafe_allow_html=True)
+    st.markdown(f'<p class="metric-value">{df_meteo["min_temp"][0]} °C</p>', unsafe_allow_html=True)
+
 with col3:
-    st.metric(f"""Risque de pluie""", f"""{df_meteo["daily_chance_of_rain"][0]} %""")
+    st.markdown('<p class="metric-title">Taux d\'humidité</p>', unsafe_allow_html=True)
+    st.markdown(f'<p class="metric-value">{df_meteo["avghumidity"][0]} %</p>', unsafe_allow_html=True)
+
 with col4:
-    st.metric("Taux d'humidité", f"{df_meteo['avghumidity'][0]} %")
+    st.markdown('<p class="metric-title">Risque de pluie</p>', unsafe_allow_html=True)
+    st.markdown(f'<p class="metric-value">{df_meteo["daily_chance_of_rain"][0]} %</p>', unsafe_allow_html=True)
 
 col1, col2, col3, col4 = st.columns(4)
-with col3:
+with col4:
     if df_meteo["daily_chance_of_rain"][0] > 0:
-        st.metric("Précipitations", f"""{df_meteo["totalprecip_mm"][0]}mm""")
+        st.markdown('<p class="metric-title">Précipitations</p>', unsafe_allow_html=True)
+        st.markdown(f'<p class="metric-value">{df_meteo["totalprecip_mm"][0]} mm</p>', unsafe_allow_html=True)
 with col3:
     if df_meteo["daily_chance_of_snow"][0] > 0:
-        st.metric("Risque de neige", f"""{df_meteo["daily_chance_of_snow"][0]} %""")
+        st.markdown('<p class="metric-title">Risque de neige</p>', unsafe_allow_html=True)
+        st.markdown(f'<p class="metric-value">{df_meteo["daily_chance_of_snow"][0]} %</p>', unsafe_allow_html=True)
 
-st.write("---")
+st.markdown("<hr style='border: 0.5px solid #57bb8a;'>", unsafe_allow_html=True)
 
 col1, col2, col3, col4 = st.columns(4)
 
@@ -140,7 +173,7 @@ col1, col2, col3, col4 = st.columns(4)
 with col2:
     st.write("")
     st.write("")
-    st.html("""<h2 style='text-align: center; color: white' > Demain </h2>""")
+    st.html("""<h2 style='text-align: center; color: white;font-family: "Economica"' > Demain </h2>""")
 
 with col3:
     st.image(df_meteo["condition_icon"][1], width=150)
@@ -148,41 +181,76 @@ with col3:
 
 col1, col2, col3, col4 = st.columns(4)
 
+# Affichage des éléments dans chaque colonne
 with col1:
-    st.metric("Temperature max", f"{df_meteo["max_temp"][1]} °C")
+    st.markdown('<p class="metric-title">Température max</p>', unsafe_allow_html=True)
+    st.markdown(f'<p class="metric-value">{df_meteo["max_temp"][1]} °C</p>', unsafe_allow_html=True)
+
 with col2:
-    st.metric("Temperature min", f"{df_meteo["min_temp"][1]} °C")
+    st.markdown('<p class="metric-title">Température min</p>', unsafe_allow_html=True)
+    st.markdown(f'<p class="metric-value">{df_meteo["min_temp"][1]} °C</p>', unsafe_allow_html=True)
+
 with col3:
-    st.metric(f"""Risque de pluie""", f"""{df_meteo["daily_chance_of_rain"][1]} %""")
+    st.markdown('<p class="metric-title">Taux d\'humidité</p>', unsafe_allow_html=True)
+    st.markdown(f'<p class="metric-value">{df_meteo["avghumidity"][1]} %</p>', unsafe_allow_html=True)
+
 with col4:
-    st.metric("Taux d'humidité", f"{df_meteo['avghumidity'][1]} %")
+    st.markdown('<p class="metric-title">Risque de pluie</p>', unsafe_allow_html=True)
+    st.markdown(f'<p class="metric-value">{df_meteo["daily_chance_of_rain"][1]} %</p>', unsafe_allow_html=True)
 
 col1, col2, col3, col4 = st.columns(4)
-with col3:
+with col4:
     if df_meteo["daily_chance_of_rain"][1] > 0:
-        st.metric("Précipitations", f"""{df_meteo["totalprecip_mm"][1]}mm""")
-with col4:
+        st.markdown('<p class="metric-title">Précipitations</p>', unsafe_allow_html=True)
+        st.markdown(f'<p class="metric-value">{df_meteo["totalprecip_mm"][1]} mm</p>', unsafe_allow_html=True)
+with col3:
     if df_meteo["daily_chance_of_snow"][1] > 0:
-        st.metric("Risque de neige", f"""{df_meteo["daily_chance_of_snow"][1]} %""")
+        st.markdown('<p class="metric-title">Risque de neige</p>', unsafe_allow_html=True)
+        st.markdown(f'<p class="metric-value">{df_meteo["daily_chance_of_snow"][1]} %</p>', unsafe_allow_html=True)
 
-st.write("---")
+st.markdown("<hr style='border: 0.5px solid #57bb8a;'>", unsafe_allow_html=True)
 
 col1, col2, col3, col4 = st.columns(4)
 
-list_col = [col1, col2, col3, col4]
 
-for i, col in enumerate(list_col):
-    with col:
-        st.html(
-            f"""<p style='text-align: center; color: white; font-size: 18px; margin-bottom: 0' > {df_meteo["jour_fr"][i+2]} </p>"""
-        )
-        st.html(
-            f"""<div style = "text-align : center;"> <img style = "height: 150px" src = "{df_meteo["condition_icon"][i+2]}" </div>"""
-        )
-        st.html(
-            f"""<p style='text-align: center; color: white' > <strong>{df_meteo['max_temp'][i+2]} °</strong>  <i> {df_meteo['min_temp'][i+2]} °</i></p>"""
-        )
+with col2:
+    st.write("")
+    st.write("")
+    st.html(f"""<h2 style='text-align: center; color: white;font-family: "Economica"' > {df_meteo['jour_fr'][2]} </h2>""")
 
+with col3:
+    st.image(df_meteo["condition_icon"][2], width=150)
+
+col1, col2, col3, col4 = st.columns(4)
+
+# Affichage des éléments dans chaque colonne
+with col1:
+    st.markdown('<p class="metric-title">Température max</p>', unsafe_allow_html=True)
+    st.markdown(f'<p class="metric-value">{df_meteo["max_temp"][2]} °C</p>', unsafe_allow_html=True)
+
+with col2:
+    st.markdown('<p class="metric-title">Température min</p>', unsafe_allow_html=True)
+    st.markdown(f'<p class="metric-value">{df_meteo["min_temp"][2]} °C</p>', unsafe_allow_html=True)
+
+with col3:
+    st.markdown('<p class="metric-title">Taux d\'humidité</p>', unsafe_allow_html=True)
+    st.markdown(f'<p class="metric-value">{df_meteo["avghumidity"][2]} %</p>', unsafe_allow_html=True)
+
+with col4:
+    st.markdown('<p class="metric-title">Risque de pluie</p>', unsafe_allow_html=True)
+    st.markdown(f'<p class="metric-value">{df_meteo["daily_chance_of_rain"][2]} %</p>', unsafe_allow_html=True)
+
+col1, col2, col3, col4 = st.columns(4)
+with col4:
+    if df_meteo["daily_chance_of_rain"][2] > 0:
+        st.markdown('<p class="metric-title">Précipitations</p>', unsafe_allow_html=True)
+        st.markdown(f'<p class="metric-value">{df_meteo["totalprecip_mm"][2]} mm</p>', unsafe_allow_html=True)
+with col3:
+    if df_meteo["daily_chance_of_snow"][2] > 0:
+        st.markdown('<p class="metric-title">Risque de neige</p>', unsafe_allow_html=True)
+        st.markdown(f'<p class="metric-value">{df_meteo["daily_chance_of_snow"][2]} %</p>', unsafe_allow_html=True)
+
+st.markdown("<hr style='border: 0.5px solid #57bb8a;'>", unsafe_allow_html=True)
 
 ##### Graphique précipitations
 
@@ -192,11 +260,23 @@ fig = px.area(
     x="jour_fr",
     y="totalprecip_mm",
     labels=False,
-    title="Prévisions des précipitations (en mm)",
+    title="Prévisions des Précipitations (en mm)",
 )
-fig.update_layout(xaxis_title=None, yaxis_title=None)
+fig.update_layout(
+    title={
+        'text': "Prévisions des Précipitations (en mm)",
+        'font': {
+            'family': 'Open Sans',  # Police choisie
+            'size': 24,             # Taille du titre
+        },
+        'x': 0.5,                  # Centrer le titre
+        'xanchor': 'center'       # Ancrage du titre
+    },
+    xaxis_title=None,
+    yaxis_title=None
+)
 fig.update_traces(
-    line_color="dodgerblue",
-    fillcolor="rgba(30, 144, 255, 0.4)",  # dodgerblue en semi-transparent
+    line_color="#57bb8a",
+    fillcolor="rgba(87, 187, 138, 0.4)",  # dodgerblue en semi-transparent
 )
 st.plotly_chart(fig)
